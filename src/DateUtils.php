75b9154de7a698e7e5bf608f2866a2134c9019a3
Parables <38154990+Parables@users.noreply.github.com>
2025-06-06 07:06:38 +0000

    fix: explicitly mark nullable params as null

--- src/DateUtils.php
+++ src/DateUtils.php
@@ -4,14 +4,14 @@ namespace Parables\Utils;
 
 trait DateUtils
 {
-    public static function excelDateToPhpDate(int $excelDate = null): ?string
-    {
-        if (empty($excelDate)) {
-            return null;
-        }
-
-        $phpDate = $excelDate - 25569;  // to offset to Unix epoch
-
-        return date(format: 'Y-m-d', timestamp: strtotime("+$phpDate days", mktime(0, 0, 0, 1, 1, 1970)));
+  public static function excelDateToPhpDate(int|null $excelDate = null): ?string
+  {
+    if (empty($excelDate)) {
+      return null;
     }
+
+    $phpDate = $excelDate - 25569;  // to offset to Unix epoch
+
+    return date(format: 'Y-m-d', timestamp: strtotime("+$phpDate days", mktime(0, 0, 0, 1, 1, 1970)));
+  }
 }
